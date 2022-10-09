1cfdb801af7e70909196fbb76f8cf57672573c0e
Christian Lee <111724223+chjlee1@users.noreply.github.com>
2022-10-09 02:00:53 -0400

    main.py updated button labels

--- main.py
+++ main.py
@@ -9,22 +9,16 @@ df_census = pd.read_csv(r'sf_census.csv')     #this is the dataset we found that
 df_census_i = df_census.set_index('GEOID')
 df_flood_i = df_flood.set_index('Census Blockgroup')
 
-#df_flood.shape
-#df_census.shape
-col1, col2 = st.columns(2)
-
+df_flood.shape
+df_census.shape
 
 df_main = df_flood.merge(df_census, left_on='Census Blockgroup', right_on='GEOID')    #merging datasets and dropping unneeded features
 df_main = df_main.drop(["data_as_of", "data_loaded_at", "multipolygon"], axis=1)
-df_main = df_main[['Census Blockgroup', 'INTPTLAT', 'INTPTLON', 'Children', 'Elderly', 'NonWhite', 'Poverty', 'Education', 'English', 'Elevation', 'SeaLevelRise', 'Precipitation', 'Diabetes', 'MentalHealth', 'Asthma', 'Disability', 'HousingQuality', 'Homeless', 'LivAlone', 'FloodHealthIndex', 'FloodHealthIndex_Quintiles']]
-df_main.rename(columns={'INTPTLAT': 'lat', 'INTPTLON': 'lon'}, inplace=True)
-df_main['coordinates'] = df_main.apply(lambda x: (x.lon, x.lat), axis=1) #idk what this line does lol
-
-
+# df_main = df_main[['Census Blockgroup', 'INTPTLAT', 'INTPTLON', 'Children', 'Elderly', 'NonWhite', 'Poverty', 'Education', 'English', 'Elevation', 'SeaLevelRise', 'Precipitation', 'Diabetes', 'MentalHealth', 'Asthma', 'Disability', 'HousingQuality', 'Homeless', 'LivAlone', 'FloodHealthIndex', 'FloodHealthIndex_Quintiles']]
 df_button = df_main[['Children', 'Elderly', 'NonWhite', 'Poverty', 'Education', 'English', 'SeaLevelRise', 'Precipitation', 'Diabetes', 'MentalHealth', 'Asthma', 'Disability', 'HousingQuality', 'Homeless', 'LivAlone', 'FloodHealthIndex']]
-button_options = ['Children', 'Elderly', 'NonWhite', 'Poverty', 'Education', 'English', 'SeaLevelRise', 'Precipitation', 'Diabetes', 'MentalHealth', 'Asthma', 'Disability', 'HousingQuality', 'Homeless', 'LivAlone', 'FloodHealthIndex']
+button_options = ['Residents under age 18', 'Residents age 65 or over', 'Minority residents (not Hispanic or Latino)', 'Individuals under 200% of poverty rate', 'Individuals over age 25 with high school degree', 'Households that are non-English speaking', 'Land area with 36-inches of sea-level rise', 'Land area with over 6-inches of projected precipitation', 'Diabetes hospitalization rate of adults', 'Mental Health related hospitalization rate', 'Asthma hospitalization rate of adults', 'Residents with a disability', 'Annual housing violations', 'Homeless population', 'Residents who live alone', 'Flood Health Vulnerability Index']
 option = st.selectbox(
-    label="map display",
+    label="Percentage of:",
     options=button_options
 )
 '''
@@ -32,151 +26,53 @@ The column labels are not great as button labels, as they are not very descripti
 also don't have spaces between the words. I can change the button labels later I am just
 dropping this here to remind myself.
 '''
+str_options = ['Children', 'Elderly', 'NonWhite', 'Poverty', 'Education', 'English', 'SeaLevelRise', 'Precipitation', 'Diabetes', 'MentalHealth', 'Asthma', 'Disability', 'HousingQuality', 'Homeless', 'LivAlone', 'FloodHealthIndex']
+x = 0
+for i in button_options:
+  if i == option:
+    break
+  x += 1
+df_main = df_main[['INTPTLAT', 'INTPTLON', str_options[x]]]
+df_main.rename(columns={'INTPTLAT': 'lat', 'INTPTLON': 'lon'}, inplace=True)
 
-df_option = df_main[['lat', 'lon','coordinates', option]]
+df_main['coordinates'] = df_main.apply(lambda x: (x.lon, x.lat), axis=1) #idk what this line does lol
 
-midpoint = (np.average(df_option["lat"]), np.average(df_option["lon"]))
-df_option['circle_radius'] = df_option[option] * 100
+st.dataframe(df_main)
+#st.map(df_main) #2d map (commented out for now i think 3d is better
 
-df_zone1 = df_main[df_main['FloodHealthIndex_Quintiles'] == 1]
-df_zone2 = df_main[df_main['FloodHealthIndex_Quintiles'] == 2]
-df_zone3 = df_main[df_main['FloodHealthIndex_Quintiles'] == 3]
-df_zone4 = df_main[df_main['FloodHealthIndex_Quintiles'] == 4]
-df_zone5 = df_main[df_main['FloodHealthIndex_Quintiles'] == 5]
+midpoint = (np.average(df_main["lat"]), np.average(df_main["lon"]))
+df_main['circle_radius'] = df_main[str_options[x]] * 100
 
+st.write(
+    pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": midpoint[0],
+            "longitude": midpoint[1],
+            "zoom": 11,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "ScatterplotLayer",
+                df_main,
+                pickable=True,
+                opacity=1.0,
+                stroked=True,
+                filled=True,
+                radius_scale=6,
+                radius_min_pixels=3,
+                radius_max_pixels=100,
+                line_width_min_pixels=1,
+                get_position="coordinates",
+                get_radius="circle_radius",
+                get_fill_color=[255, 0, 0],
+                get_line_color=[0, 0, 0],
+            ),
+        ],
+    )
+)
 
-r = (
-  pdk.Deck(
-    map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state={
-        "latitude": midpoint[0],
-        "longitude": midpoint[1],
-        "zoom": 11,
-        "pitch": 50,
-    },
-    layers=[
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone5,
-        pickable=True,
-        opacity=0.0,
-        stroked=True,
-        filled=True,
-        radius_scale=6,
-        radius_min_pixels=3,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[255, 0, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone4,
-        pickable=True,
-        opacity=0.05,
-        stroked=False,
-        filled=True,
-        radius_scale=7,
-        radius_min_pixels=10,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[255, 0, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone3,
-        pickable=True,
-        opacity=0.05,
-        stroked=False,
-        filled=True,
-        radius_scale=7,
-        radius_min_pixels=10,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[255, 153, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone2,
-        pickable=True,
-        opacity=0.05,
-        stroked=False,
-        filled=True,
-        radius_scale=7,
-        radius_min_pixels=10,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[255, 255, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone1,
-        pickable=True,
-        opacity=0.05,
-        stroked=False,
-        filled=True,
-        radius_scale=7,
-        radius_min_pixels=10,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[153, 255, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_zone5,
-        pickable=True,
-        opacity=0.05,
-        stroked=False,
-        filled=True,
-        radius_scale=7,
-        radius_min_pixels=10,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[0, 255, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_option,
-        pickable=True,
-        opacity=1.0,
-        stroked=True,
-        filled=True,
-        radius_scale=6,
-        radius_min_pixels=3,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius="circle_radius",
-        get_fill_color=[255, 0, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      
-    ],
-))
-
-
-with col1:
-    st.dataframe(df_option)
-    #st.map(df_main) #2d map (commented out for now i think 3d is better
-with col2:
-    st.pydeck_chart(r)
 
 
 @st.cache
@@ -191,4 +87,3 @@ st.download_button(
     file_name='sf_main.csv',
     mime='text/csv',
 )
-
