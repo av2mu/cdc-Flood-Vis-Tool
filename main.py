7e2c7e13c89054e5cca3e03e6fdca46952c98caf
aidanmaguire <aidanbmaguire@gmail.com>
2022-10-09 01:04:13 -0400

    fixed radii of circles and added columns

--- main.py
+++ main.py
@@ -35,91 +35,85 @@ df_main.rename(columns={'INTPTLAT': 'lat', 'INTPTLON': 'lon'}, inplace=True)
 df_main['coordinates'] = df_main.apply(lambda x: (x.lon, x.lat), axis=1) #idk what this line does lol
 with col1:
     st.dataframe(df_main)
-    #st.map(df_main) #2d map (commented out for now i think 3d is better
-st.dataframe(df_main)
-
-df_zone1 = df_main[df_main['FloodHealthIndex_Quintiles'] == 1]
-
-df_zone1
 
 midpoint = (np.average(df_main["lat"]), np.average(df_main["lon"]))
-df_main['circle_radius'] = df_main[option] * 100
+match option:
+    case 'Children':
+        df_main['circle_radius'] = df_main[option] * 100
+    case "Elderly":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "NonWhite":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "Poverty":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "Education":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "English":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "SeaLevelRise":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "Precipitation":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "Diabetes":
+        df_main['circle_radius'] = df_main[option]
+    case "MentalHealth":
+        df_main['circle_radius'] = df_main[option]
+    case "Asthma":
+        df_main['circle_radius'] = df_main[option]
+    case "Disability":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "HousingQuality":
+        df_main['circle_radius'] = df_main[option]
+    case "Homeless":
+        df_main['circle_radius'] = df_main[option]
+    case "LivAlone":
+        df_main['circle_radius'] = df_main[option] * 100
+    case "FloodHealthIndex":
+        df_main['circle_radius'] = df_main[option] * .75
 
 with col2:
     st.write(
-        pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={
-                "latitude": midpoint[0],
-                "longitude": midpoint[1],
-                "zoom": 11,
-                "pitch": 50,
-            },
-            layers=[
-                pdk.Layer(
-                    "ScatterplotLayer",
-                    df_main,
-                    pickable=True,
-                    opacity=1.0,
-                    stroked=True,
-                    filled=True,
-                    radius_scale=6,
-                    radius_min_pixels=3,
-                    radius_max_pixels=100,
-                    line_width_min_pixels=1,
-                    get_position="coordinates",
-                    get_radius="circle_radius",
-                    get_fill_color=[255, 0, 0],
-                    get_line_color=[0, 0, 0],
-                ),
-            ],
-        )
-    )
+      pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": midpoint[0],
+            "longitude": midpoint[1],
+            "zoom": 11,
+            "pitch": 50,
+        },
+        layers=[
+          pdk.Layer(
+            "ScatterplotLayer",
+            df_main,
+            pickable=True,
+            opacity=1.0,
+            stroked=True,
+            filled=True,
+            radius_scale=6,
+            radius_min_pixels=3,
+            radius_max_pixels=100,
+            line_width_min_pixels=1,
+            get_position="coordinates",
+            get_radius= "circle_radius",
+            get_fill_color=[255, 0, 0],
+            get_line_color=[0, 0, 0],
+          ),
+          pdk.Layer(
+            "HexagonLayer",
+            #df_zone1,
+            get_position=["lon", "lat"],
+            auto_highlight=True,
+            elevation_scale=50,
+            pickable=True,
+            elevation_range=[0, 3000],
+            extruded=False,
+            coverage=1,
+            radius = 70,
+            opacity = 0.5,
+          )
 
-
-
-st.write(
-  pdk.Deck(
-    map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state={
-        "latitude": midpoint[0],
-        "longitude": midpoint[1],
-        "zoom": 11,
-        "pitch": 50,
-    },
-    layers=[
-      pdk.Layer(
-        "ScatterplotLayer",
-        df_main,
-        pickable=True,
-        opacity=1.0,
-        stroked=True,
-        filled=True,
-        radius_scale=6,
-        radius_min_pixels=3,
-        radius_max_pixels=100,
-        line_width_min_pixels=1,
-        get_position="coordinates",
-        get_radius= "circle_radius",
-        get_fill_color=[255, 0, 0],
-        get_line_color=[0, 0, 0],
-      ),
-      pdk.Layer(
-        "HexagonLayer",
-        df_zone1,
-        get_position=["lon", "lat"],
-        auto_highlight=True,
-        elevation_scale=50,
-        pickable=True,
-        elevation_range=[0, 3000],
-        extruded=False,
-        coverage=1,
-        radius = 70,
-        opacity = 0.5,
-      )
-      
-    ],
-))
+        ],
+    ))
 
 @st.cache
 def convert_df_to_csv(df):
